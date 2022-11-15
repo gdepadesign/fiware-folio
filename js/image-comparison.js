5b33db7b10bf12adfc1a2307c8f1a9d81078ba17
Giacomo <reddy.depa@gmail.com>
2022-11-15 15:37:41 +0100

    js update

--- js/image-comparison.js
+++ js/image-comparison.js
@@ -21,10 +21,10 @@ slider = new juxtapose.JXSlider(
   "#juxtapose-2",
   [
     {
-      src: "./images/fm_01.png",
+      src: "./images/fm_04.png",
     },
     {
-      src: "./images/fm_02.png",
+      src: "./images/fm_03.png",
     },
   ],
   {
@@ -40,10 +40,10 @@ slider = new juxtapose.JXSlider(
   "#juxtapose-3",
   [
     {
-      src: "./images/fm_01.png",
+      src: "./images/fm_06.png",
     },
     {
-      src: "./images/fm_02.png",
+      src: "./images/fm_05.png",
     },
   ],
   {
